0c018630f633eb7ce539f28fe3d9b7dd66fc40e3
stoicspark49-ui <stoicspark49@gmail.com>
2026-04-30 10:37:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-r
+// Protect page
+checkAuth();
+
+// Navigation
+function switchPage(page, el) {
+  document.querySelectorAll(".page").forEach(p => p.classList.remove("active"));
+  document.getElementById("page-" + page).classList.add("active");
+
+  document.querySelectorAll(".nav-tab").forEach(t => t.classList.remove("active"));
+  el.classList.add("active");
+}
